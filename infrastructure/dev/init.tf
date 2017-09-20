c5e3489befc2f957ded9fd04544f32eea725a68f
epy0n0ff <epy0n.0ff@gmail.com>
2017-09-20 19:35:30 +0900

    [don't include] test env

--- infrastructure/dev/init.tf
+++ infrastructure/dev/init.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "incident-app-team-a-tfstate"
+    bucket = "incident-app-team-a-tfstate-epy0n0ff"
     key    = "incident-app-team-a-dev.tfstate"
     region = "ap-northeast-1"
   }
