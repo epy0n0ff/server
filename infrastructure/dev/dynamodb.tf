63313d063f4435939706e458d3bfef50cb91edcc
epy0n0ff <epy0n.0ff@gmail.com>
2017-09-25 17:31:10 +0900

    Add call history table definition

--- infrastructure/dev/dynamodb.tf
+++ infrastructure/dev/dynamodb.tf
@@ -43,3 +43,33 @@ resource "aws_dynamodb_table" "alert_table" {
     Environment = "development"
   }
 }
+
+resource "aws_dynamodb_table" "call_history_table" {
+  name           = "CallHistory"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "IncidentId"
+  range_key      = "CreatedAt"
+
+  local_secondary_index {
+    name            = "IncidentIdCognitoIdIndex"
+    range_key       = "CognitoId"
+    projection_type = "ALL"
+  }
+
+  local_secondary_index {
+    name            = "IncidentIdActionIndex"
+    range_key       = "Action"
+    projection_type = "ALL"
+  }
+
+  attribute {
+    name = "IncidentId"
+    type = "N"
+  }
+
+  attribute {
+    name = "CreatedAt"
+    type = "N"
+  }
+}
