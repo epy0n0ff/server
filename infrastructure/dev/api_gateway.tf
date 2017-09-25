bcfda592a67118de56d0451265a59628975e62b9
epy0n0ff <epy0n.0ff@gmail.com>
2017-09-25 18:00:37 +0900

    Add kms policy for lambda functions

--- infrastructure/dev/api_gateway.tf
+++ infrastructure/dev/api_gateway.tf
@@ -8,3 +8,50 @@ resource "aws_iam_role_policy_attachment" "dynamodb-lambda" {
   role       = "${element(split("/",var.apex_function_role), 1)}"
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
+
+resource "aws_iam_policy" "kms-lambda-policy" {
+  name        = "kms-lambda-policy"
+  description = "kms policy for lambda function"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "kms:CreateAlias",
+                "kms:CreateKey",
+                "kms:DisableKey",
+                "kms:DeleteAlias",
+                "kms:Describe*",
+                "kms:DescribeKey",
+                "kms:EnableKey",
+                "kms:Encrypt",
+                "kms:GenerateRandom",
+                "kms:Get*",
+                "kms:List*",
+                "kms:ScheduleKeyDeletion",
+                "kms:TagResource",
+                "kms:UntagResource",
+                "kms:UpdateKeyDescription",
+                "iam:ListGroups",
+                "iam:ListRoles",
+                "iam:ListUsers"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "kms-lambda" {
+  role       = "${element(split("/",var.apex_function_role), 1)}"
+  policy_arn = "${aws_iam_policy.kms-lambda-policy.arn}"
+}
+
+resource "aws_kms_key" "plivo" {
+  description = "plivo kms key"
+  is_enabled  = true
+}
