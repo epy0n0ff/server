b18827fa3eb037c2d4661d0d3a1a0ae5c0a5b053
epy0n0ff <epy0n.0ff@gmail.com>
2017-09-28 16:34:41 +0900

    Add application/json request template pattern

--- infrastructure/dev/api_gateway_plivo.tf
+++ infrastructure/dev/api_gateway_plivo.tf
@@ -43,6 +43,51 @@ EOF
   }
 }
 
+variable "plivo_request_templates_json" {
+  type = "map"
+
+  default = {
+    "application/json" = <<EOF
+#set($allParams = $input.params())
+{
+"body-json" : $input.json('$'),
+"params" : {
+#foreach($type in $allParams.keySet())
+    #set($params = $allParams.get($type))
+"$type" : {
+    #foreach($paramName in $params.keySet())
+    "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
+        #if($foreach.hasNext),#end
+    #end
+}
+    #if($foreach.hasNext),#end
+#end
+},
+"context" : {
+    "account-id" : "$context.identity.accountId",
+    "api-id" : "$context.apiId",
+    "api-key" : "$context.identity.apiKey",
+    "authorizer-principal-id" : "$context.authorizer.principalId",
+    "caller" : "$context.identity.caller",
+    "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
+    "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
+    "cognito-identity-id" : "$context.identity.cognitoIdentityId",
+    "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
+    "http-method" : "$context.httpMethod",
+    "stage" : "$context.stage",
+    "source-ip" : "$context.identity.sourceIp",
+    "user" : "$context.identity.user",
+    "user-agent" : "$context.identity.userAgent",
+    "user-arn" : "$context.identity.userArn",
+    "request-id" : "$context.requestId",
+    "resource-id" : "$context.resourceId",
+    "resource-path" : "$context.resourcePath"
+    }
+}
+EOF
+  }
+}
+
 #
 # /plivo
 #
@@ -61,7 +106,7 @@ module "plivo_makecall" {
   rest_api_id          = "${aws_api_gateway_rest_api.incident-app-team-a.id}"
   apex_function_arns   = "${var.apex_function_arns}"
   lambda_function_name = "post_makecall"
-  request_templates    = "${var.plivo_request_templates}"
+  request_templates    = "${var.plivo_request_templates_json}"
 }
 
 #
